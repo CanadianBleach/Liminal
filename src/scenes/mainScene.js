10922f1bd2eb13dbc53011d25954ec45e8154aa5
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2025-05-07 20:22:04 -0400

    Changed flashlight

--- src/scenes/mainScene.js
+++ src/scenes/mainScene.js
@@ -144,10 +144,6 @@ function setupEvents(camera, renderer, controls) {
         }
     });
 
-    document.addEventListener('pointerdown', (e) => {
-        if (e.button === 2) toggleFlashlight();
-    });
-
     document.addEventListener('contextmenu', (e) => e.preventDefault());
 }
 
