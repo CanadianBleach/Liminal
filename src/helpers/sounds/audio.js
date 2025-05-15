c240e9cc3b7cae0b5925a2b9607e502cf485ea5e
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2025-05-15 11:00:44 -0400

    Gun sounds

--- src/helpers/sounds/audio.js
+++ src/helpers/sounds/audio.js
@@ -17,6 +17,9 @@ export function loadSounds(camera) {
   loadSound('jump', '/sounds/jump.mp3');
   loadSound('dive', '/sounds/dive.mp3');
   loadSound('vine_boom', '/sounds/vine_boom.mp3');
+  loadSound('gunshot_1', './sounds/gunshot_1.MP3');
+  loadSound('gunshot_2', './sounds/gunshot_2.MP3');
+  loadSound('gunshot_3', './sounds/gunshot_3.MP3');
 
   return soundBuffers;
 }
